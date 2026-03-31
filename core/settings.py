80f2309f91e2f7b8c75c6fde61f84b5eab4d8379
kimberlynjoroge <kimberlynjoroge02@gmail.com>
2026-04-01 02:27:04 +0300

    changes to settings.py

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7h@242ic#qsxod#i-6aa3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kimbia-eldoret.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
